5389d4ea015db8e7c6b47ac5c9f750efaf8194fc
Rakesh Kumar Das <62099358+Rakeshpythonista@users.noreply.github.com>
2021-05-01 23:37:47 +0530

    Update BulkFileRenaming.py

--- BulkFileRenaming.py
+++ BulkFileRenaming.py
@@ -1,15 +1,11 @@
 import os
 
-def main():
-    i = 0
-    path = "D:/Rakesh_GIT/PythonSmallProjects/FacebookGIETImages/"
-    for filename in os.listdir(path):
-        new_name = "GIET" + str(i) + ".jpg"
-        my_source =path + filename
-        new_name =path + new_name
-        os.rename(my_source , new_name)
-        i += 1
+path = "D:/Rakesh_GIT/PythonSmallProjects/FacebookGIETImages/"
 
-if __name__ == '__main__':
-    main()
+i = 0
+for filename in os.listdir(path):
+    new_name = "GIET{i}.jpg".format(i)
+    os.rename(filename , new_name)
+    i += 1
+
 
